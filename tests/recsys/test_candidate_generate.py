2acc3491ce7f591d29714ea6b46fe064211a8baa
zerebom <ice.choco.pudding.kokoro@gmail.com>
2023-12-11 18:51:25 +0900

    Add test

--- tests/recsys/test_candidate_generate.py
+++ tests/recsys/test_candidate_generate.py
@@ -1,10 +1,10 @@
 import polars as pl
 
-from scorta.recsys.candidate_generate import SampleCandidate
+from scorta.recsys.candidate_generate import Evaluator, SampleCandidate
 
 
 class TestSampleCandidate:
-    def test_generate(self):
+    def test_generate(self) -> None:
         # テスト用のインスタンスを作成
         candidate_generator = SampleCandidate(output_dir="test_output")
 
@@ -16,7 +16,37 @@ class TestSampleCandidate:
         assert generated_df.shape[0] == 9, "生成されたDataFrameの行数は9であるべきです"
         assert "score" in generated_df.columns, "'score'列がDataFrameに含まれているべきです"
 
-    def test_save(self):
+    def test_save(self) -> None:
         candidate = SampleCandidate(output_dir="test_output")
         candidate.save(candidate.generate())
         assert candidate.output_path.exists(), "saveメソッドで指定したパスにファイルが生成されているべきです"
+
+
+class TestEvaluator:
+    def test_evaluate(self) -> None:
+        k = 1
+        # テスト用のDataFrameを作成
+        target_df = pl.DataFrame(
+            {
+                "user_id": [1, 1, 2],
+                "item_id": [1, 2, 1],
+                "target": [1, 0, 1],
+            }
+        )
+        evaluator = Evaluator(evaluate_topks=[k], target_df=target_df)
+        df = pl.DataFrame(
+            {
+                "user_id": [1, 1, 1, 2, 2, 2, 3, 3, 3],
+                "item_id": [1, 2, 3, 1, 2, 3, 1, 2, 3],
+                "score": [0.1, 0.2, 0.3, 0.2, 0.4, 0.6, 0.3, 0.6, 0.9],
+                "rank": [1, 2, 3, 1, 2, 3, 1, 2, 3],
+            }
+        )
+
+        # evaluateメソッドを呼び出し
+        result = evaluator.evaluate(df, "test_candidate")
+
+        # 期待通りの結果が得られているか確認
+        assert isinstance(result, dict), "evaluateメソッドはdictを返すべきです"
+        assert "cg_name" in result[k], "ndcgが結果に含まれているべきです"
+        assert f"precision@{k}" in result[k]["eval_dic"][1], "precisionが結果に含まれているべきです"
